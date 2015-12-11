4beb013b3c0c7d9b662f6de93a70329110312d3c
Zhou Zejin <zjzhou1@chinaums.com>
2015-12-11 18:01:08 +0800

    完成12.3.2 创建气象站。

--- gen/com/sunny/weatherstation/R.java
+++ gen/com/sunny/weatherstation/R.java
@@ -25,7 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int light=0x7f080002;
+        public static final int pressure=0x7f080001;
+        public static final int temperature=0x7f080000;
     }
     public static final class layout {
         public static final int activity_weather_station=0x7f030000;
@@ -34,9 +37,8 @@ public final class R {
         public static final int weather_station=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
--- src/com/sunny/weatherstation/WeatherStationActivity.java
+++ src/com/sunny/weatherstation/WeatherStationActivity.java
@@ -1,34 +1,148 @@
 package com.sunny.weatherstation;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class WeatherStationActivity extends Activity {
+
+	private SensorManager sensorManager;
+	private TextView temperatureTextView;
+	private TextView pressureTextView;
+	private TextView lightTextView;
+
+	private float currentTemperature = Float.NaN;
+	private float currentPressure = Float.NaN;
+	private float currentLight = Float.NaN;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_weather_station);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.weather_station, menu);
-		return true;
+		temperatureTextView = (TextView) findViewById(R.id.temperature);
+		pressureTextView = (TextView) findViewById(R.id.pressure);
+		lightTextView = (TextView) findViewById(R.id.light);
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		
+		Timer updateTimer = new Timer("weatherUpdate");
+		updateTimer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				updateGUI();
+			}
+		}, 0, 100);
 	}
+	
+	private final SensorEventListener tempSensorEventListener = new SensorEventListener() {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			currentTemperature = event.values[0];
 		}
-		return super.onOptionsItemSelected(item);
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+	
+	private final SensorEventListener pressureSensorEventListener = new SensorEventListener() {
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			currentPressure = event.values[0];
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+	
+	private final SensorEventListener lightSensorEventListener = new SensorEventListener() {
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			currentLight = event.values[0];
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+		if (lightSensor != null)
+			sensorManager.registerListener(lightSensorEventListener,
+					lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		else
+			lightTextView.setText("Light Sensor Unavailable");
+
+		Sensor pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+		if (pressureSensor != null)
+			sensorManager.registerListener(pressureSensorEventListener,
+					pressureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		else
+			pressureTextView.setText("Barometer Unavailable");
+
+		Sensor temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+		if (temperatureSensor != null)
+			sensorManager.registerListener(tempSensorEventListener,
+					temperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		else
+			temperatureTextView.setText("Thermometer Unavailable");
 	}
+
+	@Override
+	protected void onPause() {
+		sensorManager.unregisterListener(pressureSensorEventListener);
+		sensorManager.unregisterListener(tempSensorEventListener);
+		sensorManager.unregisterListener(lightSensorEventListener);
+		super.onPause();
+	}
+	
+	private void updateGUI() {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				if (!Float.isNaN(currentPressure)) {
+					pressureTextView.setText(currentPressure + "hPa");
+					pressureTextView.invalidate();
+				}
+				if (!Float.isNaN(currentLight)) {
+					String lightStr = "Sunny";
+					if (currentLight <= SensorManager.LIGHT_CLOUDY)
+						lightStr = "Night";
+					else if (currentLight <= SensorManager.LIGHT_OVERCAST)
+						lightStr = "Cloudy";
+					else if (currentLight <= SensorManager.LIGHT_SUNLIGHT)
+						lightStr = "Overcast";
+					lightTextView.setText(lightStr);
+					lightTextView.invalidate();
+				}
+				if (!Float.isNaN(currentTemperature)) {
+					temperatureTextView.setText(currentTemperature + "C");
+					temperatureTextView.invalidate();
+				}
+			}
+		});
+	};
+
 }
